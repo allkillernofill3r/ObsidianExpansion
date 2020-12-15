6b48dfc8d8736b55b94184268c8fd7a4fc99d0bd
allkillernofiller <74217462+allkillernofill3r@users.noreply.github.com>
2020-12-15 14:30:44 -0500

    Added Silk Touch to Obsidian and Crying Obsidian Glass blocks

--- src/main/java/com/allkillernofiller/obsidianexpansion/ObsidianExpansion.java
+++ src/main/java/com/allkillernofiller/obsidianexpansion/ObsidianExpansion.java
@@ -2,7 +2,6 @@ package com.allkillernofiller.obsidianexpansion;
 
 import com.allkillernofiller.obsidianexpansion.init.BlockInit;
 import com.allkillernofiller.obsidianexpansion.init.ItemInit;
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.ItemGroup;
